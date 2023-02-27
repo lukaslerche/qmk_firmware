81c94994b1b602486f1fe5150f4696cfbf63987b
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-02-27 23:39:39 +0100

    Updated key names, added reboot & some shortcuts

--- keyboards/ferris/keymaps/lukaslerche/keymap.c
+++ keyboards/ferris/keymaps/lukaslerche/keymap.c
@@ -14,16 +14,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 	DE_Q, DE_W, DE_E, DE_R, DE_T,         		                DE_Z, DE_U, DE_I,    DE_O,   DE_P,
 	DE_A, DE_S, DE_D, DE_F, DE_G,         		                DE_H, DE_J, DE_K,    DE_L,   KC_RSFT,
-	DE_Y, DE_X, DE_C, DE_V, DE_B,         		                DE_N, DE_M, DE_COMM, DE_DOT, KC_RCTRL,
+	DE_Y, DE_X, DE_C, DE_V, DE_B,         		                DE_N, DE_M, DE_COMM, DE_DOT, KC_RCTL,
 	
 	                  LCMD_T(KC_ENTER), LT(ARRNUM, KC_BACKSPACE),  LT(SYMBOL, KC_SPACE), ALGR_T(KC_TAB)
 	),
 	
 	[ARRNUM] = LAYOUT(
 	
-	KC_ESCAPE, KC_PAGE_UP, KC_UP,   KC_PAGE_DOWN, DE_GRV,         DE_ACUT, KC_7, KC_8, KC_9, KC_0,
-	KC_LCTRL,  KC_LEFT,    KC_DOWN, KC_RIGHT,     DE_DLR,         DE_TILD, KC_4, KC_5, KC_6, _______,
-	KC_1,      KC_2,       KC_3,    KC_BACKSPACE, KC_DELETE,      DE_EURO , KC_1, KC_2, KC_3, _______,
+	KC_ESCAPE,  KC_PAGE_UP,    KC_UP,          KC_PAGE_DOWN, DE_GRV,         DE_ACUT, KC_7, KC_8, KC_9, KC_0,
+	KC_LCTL,    KC_LEFT,       KC_DOWN,        KC_RIGHT,     DE_DLR,         DE_TILD, KC_4, KC_5, KC_6, _______,
+	LCMD(DE_Z), LCMD(KC_LEFT), LCMD(KC_RIGHT), KC_BACKSPACE, KC_DELETE,      DE_EURO , KC_1, KC_2, KC_3, _______,
 	
 	                                 _______,      _______,       MO(MEDFUNC), _______
 	),
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 	[MEDFUNC] = LAYOUT(
 	// maybe add layer lock to make numpad onehanded an option
-	CG_TOGG, XXXXXXX, KC_MUTE, XXXXXXX, RESET,         KC_F1, KC_F2,  KC_F3,  KC_F4,  DF(GAME),
+	CG_TOGG, XXXXXXX, KC_MUTE, XXXXXXX, QK_BOOT,         KC_F1, KC_F2,  KC_F3,  KC_F4,  DF(GAME),
 	XXXXXXX, KC_VOLD, KC_MPLY, KC_VOLU, XXXXXXX,       KC_F5, KC_F6,  KC_F7,  KC_F8,  _______,
-	XXXXXXX, DE_CIRC, DE_DEG,  DE_SECT, XXXXXXX,       KC_F9, KC_F10, KC_F11, KC_F12, _______,
+	QK_RBT , DE_CIRC, DE_DEG,  DE_SECT, XXXXXXX,       KC_F9, KC_F10, KC_F11, KC_F12, _______,
 	
 	                        _______, _______,    _______, _______
 	),
